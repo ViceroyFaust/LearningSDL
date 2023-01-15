52b68802c117dcbb92aae7aca66b98e89f66a340
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2023-01-15 23:32:34 +0200

    08 Improve Texture Learning via LazyFoo
    Prior to this change, I was following a YouTube tutorial. However, that
    code was messy.
    
    This change follows LazyFoo's instructions, which are much clearer and
    are more advanced.

--- 08Textures/textures.cpp
+++ 08Textures/textures.cpp
@@ -1,95 +1,145 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
+#include <string>
 
-SDL_Texture *loadTexture(std::string filePath, SDL_Renderer *renderTarget) {
-    SDL_Texture *texture = nullptr;
-    SDL_Surface *surface = SDL_LoadBMP(filePath.c_str());
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const std::string SCREEN_TITLE = "I render PNGs with Hardware";
 
-    if (!surface) {
-        std::cout << "Picture Loading Error\n";
-        return nullptr;
+// The program window
+SDL_Window* window = nullptr;
+// The window renderer
+SDL_Renderer* renderer = nullptr;
+// The displayed texture
+SDL_Texture* texture = nullptr;
+
+// Initializes SDL. Returns true if succeeded, false if not
+bool init() {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "Video Initialization Error: " << SDL_GetError() << "\n";
+        return false;
     }
 
-    texture = SDL_CreateTextureFromSurface(renderTarget, surface);
-
-    if (!texture) {
-        std::cout << "Texture Creation Error\n";
-        return nullptr;
+    // Create the window
+    window = SDL_CreateWindow(SCREEN_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    // Check if window has been created properly
+    if (!window) {
+        std::cout << "Window Creation Error: " << SDL_GetError() << "\n";
+        return false;
     }
 
-    SDL_FreeSurface(surface);
+    // Create a randerer for window
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    // Check if renderer has been created
+    if (!renderer) {
+        std::cout << "Renderer Creation Error: " << SDL_GetError() << "\n";
+        return false;
+    }
 
-    return texture;
+    // Set color for renderer drawing operations
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xff);
+
+    // Initialize the PNG loading library
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        std::cout << "Image Library Creation Error: " << IMG_GetError() << "\n";
+        return false;
+    }
+
+
+
+    return true;
+        
+}
+
+SDL_Texture* loadTexture(std::string path) {
+    // Final texture, to return
+    SDL_Texture* newTexture = nullptr;
+    // Load image from path to surface
+    SDL_Surface* loadedImage = IMG_Load(path.c_str());
+    if (!loadedImage) {
+        std::cout << "Failed to load image " << path << ": " << SDL_GetError() << "\n";
+        return nullptr;
+    }
+   
+    // Create a texture from surface
+    newTexture = SDL_CreateTextureFromSurface(renderer, loadedImage);
+    if (!newTexture) {
+        std::cout << "Surface to Texture Conversion Error,"
+            << path << ": " << SDL_GetError() << "\n";
+        return nullptr;
+    }
+    // Free memory for unused loadedImage
+    SDL_FreeSurface(loadedImage);
+
+    return newTexture;
+}
+
+// Load pictures, etc. Returns true if succeeded, false if not
+bool loadMedia() {
+    texture = loadTexture("texture.png");
+    if (!texture)
+        return false;
+
+    return true;
+}
+
+// Clean memory before exiting the program
+void clean() {
+    // Free Texture
+    SDL_DestroyTexture(texture);
+    texture = nullptr;
+    // Destroy Window Renderer
+    SDL_DestroyRenderer(renderer);
+    renderer = nullptr;
+    // Destroy the window
+    SDL_DestroyWindow(window);
+    window = nullptr;
+
+    // Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
 }
 
 int main(int argc, char *argv[]) {
-    // Creating window variables
-    SDL_Window *window = nullptr;
-    SDL_Renderer *renderTarget = nullptr;
-    SDL_Texture *imageTexture = nullptr;
-
-    // Initialize SDL. Quit early if an error is encountered
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "Video Initialization Error: " << SDL_GetError() << "\n";
+    if(!init()) {
+        clean();
+        return -1;
+    }
+    if (!loadMedia()) {
+        clean();
         return -1;
     }
 
-    window = SDL_CreateWindow("Exit Me", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        640, 480, SDL_WINDOW_SHOWN);
-    // Quit early if an error is encountered during window creation
-    if (!window) {
-        std::cout << "Window Creation Error: " << SDL_GetError() << "\n";
-        SDL_Quit();
-        return -1;
-    }
-
-    // -1 tells to use any compatible driver. SDL_RENDERER_ACCELERATED means
-    // that this renderer will use the GPU
-    renderTarget = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderTarget) {
-        std::cout << "Renderer Activation Error: " << SDL_GetError() << "\n";
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-
-    imageTexture = loadTexture("image.bmp", renderTarget);
-
-    // Check if image is loaded correctly. Exit early and cleanly if not
-    if (!imageTexture) {
-        std::cout << "Image Load Error: " << SDL_GetError() << "\n";
-        SDL_DestroyRenderer(renderTarget);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-    
     // Primary loop run flag
     bool isRunning = true;
     // Event handler
     SDL_Event ev;
 
-    SDL_RenderClear(renderTarget);
-    SDL_RenderCopy(renderTarget, imageTexture, NULL, NULL);
-    SDL_RenderPresent(renderTarget);
     // Primary loop
     while (isRunning) {
         // Event loop
         while (SDL_PollEvent(&ev) != 0) {
             // Quit button detection
-            if(ev.type == SDL_QUIT)
+            if (ev.type == SDL_QUIT) {
                 isRunning = false;
+            }
         }
-        
+
+        // Clear screen
+        SDL_RenderClear(renderer);
+        // Render texture to screen
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        // Update Screen
+        SDL_RenderPresent(renderer);
         // Prevent the program from hogging the CPU by slowing down the loop
         SDL_Delay(33);
     }
 
-    // Deallocate memory and quit SDL
-    SDL_DestroyTexture(imageTexture);
-    SDL_DestroyRenderer(renderTarget);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    clean();
 
     return 0;
 }
